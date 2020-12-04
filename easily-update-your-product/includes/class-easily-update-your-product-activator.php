08041f96bd470c4925f36f23ef6f04c42ac72d28
Rasim Yılmaz <iletisim@rasimyilmaz.com>
2020-12-04 23:39:06 +0300

    Auto generated with wppb.me

---
+++ easily-update-your-product/includes/class-easily-update-your-product-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://rasimyilmaz.com
+ * @since      1.0.0
+ *
+ * @package    Easily_Update_Your_Product
+ * @subpackage Easily_Update_Your_Product/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Easily_Update_Your_Product
+ * @subpackage Easily_Update_Your_Product/includes
+ * @author     Rasim Yılmaz <iletisim@rasimyilmaz.com>
+ */
+class Easily_Update_Your_Product_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ easily-update-your-product/includes/class-easily-update-your-product-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://rasimyilmaz.com
+ * @since      1.0.0
+ *
+ * @package    Easily_Update_Your_Product
+ * @subpackage Easily_Update_Your_Product/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Easily_Update_Your_Product
+ * @subpackage Easily_Update_Your_Product/includes
+ * @author     Rasim Yılmaz <iletisim@rasimyilmaz.com>
+ */
+class Easily_Update_Your_Product_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
